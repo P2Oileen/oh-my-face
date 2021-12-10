c388439ee7a0462096bea26100320321fe00d808
P2oileen <huangailin@hotdoodles.huangailin.ws2.hh-b.brainpp.cn>
2021-12-10 11:27:43 +0800

    add weight_dir

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ os.system('export PATH=/data/cuda/cuda-10.0/cuda/bin:$PATH \
             export LD_LIBRARY_PATH=/data/cuda/cuda-10.0/cuda/lib64:/data/cuda/cuda-10.0/cudnn/v7.6.0/lib64:$LD_LIBRARY_PATH \
             export CUDA_HOME=/data/cuda/cuda-10.0/cuda \
             export CUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME')
-os.system('rlaunch --gpu=1 --cpu=4 --memory=10240 -- python3 src/global_get_latent.py --input_dir \'{}\' --data_type \'{}\''.format('input.jpg', option_data_type))
+os.system('rlaunch --gpu=1 --cpu=4 --memory=10240 -- python3 src/global_get_latent.py --input_dir \'{}\' --data_type \'{}\' --weight_dir ./weights '.format('input.jpg', option_data_type))
 os.system('rlaunch --gpu=1 --cpu=4 --memory=10240 -- python3 src/global_run.py  --transfer_type \'face with smile\' --beta {} --alpha {}'.format(option_beta, option_alpha))
 os.system('rlaunch --gpu=1 --cpu=4 --memory=10240 -- python3 src/rife/inference_img.py --img output.jpg input_aligned.jpg --gamma {}'.format(option_gamma))
 

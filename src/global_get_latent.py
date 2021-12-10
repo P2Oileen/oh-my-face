e36a29acb4f33b89cb2bd598a669b8211165d353
P2oileen <huangailin@hotdoodles.huangailin.ws2.hh-b.brainpp.cn>
2021-12-10 11:26:42 +0800

    add weight_dir

--- src/global_get_latent.py
+++ src/global_get_latent.py
@@ -5,10 +5,11 @@ import torch
 parser = argparse.ArgumentParser(description='Process Options.')
 parser.add_argument('--input_dir', default='input.jpg', type=str)
 parser.add_argument('--data_type', default='face', type=str) #[face, cat]
+parser.add_argument('--weight_dir', default='./weights', type=str)
 args = parser.parse_args()
 
 img = cv2.imread(args.input_dir)
-latent,img = get_latent_code(img, args.data_type)
+latent,img = get_latent_code(img, args.data_type, args.weight_dir)
 latent = latent.unsqueeze(0)
 print("print aligned image:")
 print(cv2.imwrite("input_aligned.jpg",img))

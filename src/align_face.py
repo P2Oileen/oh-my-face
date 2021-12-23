b73cb8ea713205bbf2bc1408145fa668c715359b
P2oileen <huangailin@hotdoodles.huangailin.ws2.hh-b.brainpp.cn>
2021-12-23 11:34:24 +0800

    fixed align_face.py

--- src/align_face.py
+++ src/align_face.py
@@ -5,13 +5,19 @@ import torchvision
 import cv2
 from shape_predictor import align_face
 
+predictor_face = None
+predictor_cat = None
 
 def align_func(img, data_type, weight_dir):
+    global predictor_face
+    global predictor_cat
     if data_type == 'face':
-        predictor_face = dlib.shape_predictor(weight_dir + '/shape_predictor_68_face_landmarks.dat')
+        if predictor_face is None:
+            predictor_face = dlib.shape_predictor(weight_dir + '/shape_predictor_68_face_landmarks.dat')
         faces = align_face(img, predictor_face, data_type)
     else:
-        predictor_cat = dlib.shape_predictor(weight_dir + '/shape_predictor_cat.dat')
+        if predictor_cat is None:
+            predictor_cat = dlib.shape_predictor(weight_dir + '/shape_predictor_cat.dat')
         faces = align_face(img, predictor_cat, data_type)
     for i,face in enumerate(faces):
         if i > 0:
@@ -23,6 +29,6 @@ def align_func(img, data_type, weight_dir):
 
 if __name__ == "__main__":
     img = cv2.imread("cat.jpg")
-    img = align_func(img, 'cat')
+    img = align_func(img, 'cat', './weights')
     cv2.imwrite("cat_aligned.jpg", img.transpose(1, 2, 0) * 255)
 
